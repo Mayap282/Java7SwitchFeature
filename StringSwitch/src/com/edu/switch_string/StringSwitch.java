4bd241413b84e919b23315b5fea6b46bf4caf370
MayaP282 <maya.aitpune@gmail.com>
2017-07-05 16:41:42 -0700

    This is java 7 new feature,we can pass even string values in switch.

---
+++ StringSwitch/src/com/edu/switch_string/StringSwitch.java
@@ -0,0 +1,18 @@
+package com.edu.switch_string;
+
+public class StringSwitch {
+
+	public static void main(String[] args) {
+		String s = "h";
+		
+		switch(s){
+			case "h": System.out.println("hey you passed 'h', this is a java 7 new feature ");
+			break;
+			default:
+				System.out.println("String passing is not valid");
+		
+		
+		}
+	}
+
+}
